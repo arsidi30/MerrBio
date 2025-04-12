77422a6ee1a4fd9466c963bf100195ef4eb8bcba
arsidi30 <83333086+arsidi30@users.noreply.github.com>
2025-04-12 23:29:37 +0200

    Add files via upload

---
+++ Respositories/Abstract/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using FarmIt.Models.Domain;
+using FarmIt.Models.DTO;
+
+namespace FarmIt.Respositories.Abstract
+{
+    public interface ICategoryService
+    {
+        bool Add(Category model);
+        bool Update(Category model);
+        Category GetById(int id);
+        bool Delete(int id);
+        IQueryable<Category> List();
+
+
+
+
+
+    }
+}
---
+++ Respositories/Implimentation/CategoryService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using FarmIt.Models.Domain;
+using FarmIt.Respositories.Abstract;
+
+namespace FarmIt.Respositories.Implimentation
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly DatabaseContext ctx;
+        public CategoryService(DatabaseContext ctx) 
+        {
+            this.ctx = ctx;
+        }
+        public bool Add(Category model)
+        {
+            try 
+            {
+                ctx.Category.Add(model);
+                ctx.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var data = this.GetById(id);
+                if (data != null) 
+                {
+                    return false;
+                }
+                ctx.Category.Remove(data);
+                ctx.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public Category GetById(int id)
+        {
+            
+            return ctx.Category.Find(id);
+        }
+
+        public IQueryable<Category> List()
+        {
+            var data = ctx.Category.AsQueryable();
+            return data;
+        }
+
+        public bool Update(Category model)
+        {
+            try
+            {
+                ctx.Category.Update(model);
+                ctx.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
