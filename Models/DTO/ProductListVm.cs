77b8dcbd84238b0f60dbf5d760047df68da4b672
arsidi30 <83333086+arsidi30@users.noreply.github.com>
2025-04-12 23:27:08 +0200

    Add files via upload

---
+++ Models/DTO/ProductListVm.cs
@@ -0,0 +1,13 @@
+﻿using FarmIt.Models.Domain;
+
+namespace FarmIt.Models.DTO
+{
+    public class ProductListVm
+    {
+        public IQueryable<Product> ProductList { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string? Term { get; set; }
+    }
+}
---
+++ Models/Domain/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FarmIt.Models.Domain
+{
+    public class DatabaseContext : IdentityDbContext<ApplicationUser>
+    {
+        public DatabaseContext (DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<ProductCategory> ProductCategory { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+
+
+
+
+    }
+}
